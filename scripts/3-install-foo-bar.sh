92b4bae63761d63df15e1c22febd61a9193ea594
Sathish Ramani <sathish.ramani@aaqua.live>
2021-11-27 21:54:31 +0800

    nn

--- scripts/3-install-foo-bar.sh
+++ scripts/3-install-foo-bar.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 kubectl apply -f config/foobartemplate.yaml
+
+kubectl wait --namespace default \
+  --for=condition=ready pod \
+  --selector=app=foo \
+  --timeout=300s
+
+kubectl wait --namespace default \
+  --for=condition=ready pod \
+  --selector=app=bar \
+  --timeout=300s
