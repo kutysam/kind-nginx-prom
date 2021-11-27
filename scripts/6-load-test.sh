5408c44ee1d6cf2dbad7c186bd4cf01953e43d63
Sathish Ramani <sathish.ramani@aaqua.live>
2021-11-27 20:02:47 +0800

    bbb

--- scripts/6-load-test.sh
+++ scripts/6-load-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-k6 run --vus 50 --duration 120s ../config/loadtestbar.js
-k6 run --vus 50 --duration 120s ../config/loadtestfoo.js
+k6 run --vus 50 --duration 120s config/loadtestbar.js
+k6 run --vus 50 --duration 120s config/loadtestfoo.js
